5f9ac3e31587c7ba64d72d2ae2a0def75f31daa1
Chagong725 <725cha@gmail.com>
2022-11-21 19:18:29 -0700

    *

--- rabbits.py
+++ rabbits.py
@@ -1,83 +1,34 @@
-"""module docstring"""
-import operator
-import sys
+"""do_research finds the number of months
+    it will take 
+    for a researcher to run out of rabbit cages."""
+import csv
 
-def main():
+def do_research(cages, adults, babies):
+    
+    month = 1
+    adult = adults
+    baby = babies
+    total = adult + baby
+    temp = [month, ' '+str(adult), ' '+str(baby), ' '+str(total)]
 
-    book_data = []
+    with open('rabbits.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
 
-    with open(sys.argv[1], "r", encoding="utf8") as book:
-        lines = book.readlines()
+        _header = ["# Table of rabbit pairs"]
+        _column = ['Month', ' Adults', ' Babies', ' Total']
 
-        for i in lines:
-            temp = i.split('|')
-            temp[1] = int(temp[1])
-            temp[2] = temp[2].replace("\n", "")
-            book_data.append(temp)
+        writer.writerow(_header)
+        writer.writerow(_column)
+        writer.writerow(temp)
 
-    code = []
+        while total < cages:
+            month += 1 
+            baby = adult
+            adult = total
+            total = adult + baby
+    
+            writer.writerow([str(month),' '+str(adult),' '+str(baby), ' '+str(total)])
 
-
-    for i in book_data:
-        if i[2] in code:
-            continue
-        code.append(i[2])
-    code.sort()
-
-    string = [[] for _ in range(len(code))]
-    num = [[] for _ in range(len(code))]
-
-    for i in book_data:
-        for j in range(len(code)):
-            if i[2] == code[j]:
-                string[j].append(i[0])
-                num[j].append(i[1])
-
-    make_summary = ''
-    make_text = ''
-    for i in range(len(code)):
-        temp_list = []
-        avg = 0
-
-        for j in range(len(string[i])):
-            temp_list.append([string[i][j], num[i][j]])
-
-        sort_list = sorted(temp_list, key=operator.itemgetter(1))
-        min_str = len(sort_list[0][0])
-        max_str = len(sort_list[0][0])
-        min_index = 0
-        max_index = 0
-        for k in range(len(sort_list)):
-            str_len = len(sort_list[k][0])
-            avg += str_len
-            if min_str > str_len:
-                min_str = str_len
-                min_index = k
-            if max_str <= str_len:
-                max_str = str_len
-                max_index = k
-
-        make_text += (code[i] + '\n')
-
-        for lis in sort_list:
-            make_text += (lis[0] + '\n')
-
-        make_text += '-----\n'
-
-        avg /= len(sort_list)
-        make_summary += (code[i] + '\n' + 'Longest line (')
-        make_summary += (str(sort_list[max_index][1]) + '): ')
-        make_summary += (sort_list[max_index][0] + '\n')
-        make_summary += ('Shortest line (')
-        make_summary += (str(sort_list[min_index][1]) + '): ')
-        make_summary += (sort_list[min_index][0] + '\n')
-        make_summary += ('Average length: ' + str(round(avg)) + '\n\n')
-    make_text = make_text[:-6]
-    with open('novel_summary.txt', "w", encoding="utf8") as novel_summary:
-        novel_summary.write(make_summary)
-    with open('novel_text.txt', "w", encoding="utf8") as novel_text:
-        novel_text.write(make_text)
-
-if __name__ == "__main__":
-    main()
-
+            
+        writer.writerow([f'# Cages will run out in month {month}'])
+       
--- turtle.py
+++ turtle.py
@@ -1,34 +1,121 @@
-"""do_research finds the number of months
-    it will take 
-    for a researcher to run out of rabbit cages."""
-import csv
+  """module docstring turtle"""
 
-def do_research(cages, adults, babies):
-    
-    month = 1
-    adult = adults
-    baby = babies
-    total = adult + baby
-    temp = [month, ' '+str(adult), ' '+str(baby), ' '+str(total)]
+import turtle
 
-    with open('rabbits.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
+def get_dimentions(turtle, window, width, height):
+    """Fills background and changes size"""
+    window.screensize(width, height)
+    turtle.speed(10)
+    window.bgcolor("green")
+    turtle.penup()
 
-        _header = ["# Table of rabbit pairs"]
-        _column = ['Month', ' Adults', ' Babies', ' Total']
 
-        writer.writerow(_header)
-        writer.writerow(_column)
-        writer.writerow(temp)
+def draw_hexagon(turtle, color, scale):
+    """Draws a hexagon"""
+    turtle.color("black")
+    turtle.pendown()
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    count = 0
+    while count < 6:
+        turtle.forward(100 * scale)
+        turtle.lt(60)
+        count += 1
+    turtle.end_fill()
+    turtle.penup()
 
-        while total < cages:
-            month += 1 
-            baby = adult
-            adult = total
-            total = adult + baby
-    
-            writer.writerow([str(month),' '+str(adult),' '+str(baby), ' '+str(total)])
+def draw_star(turtle):
+    """ Draws a star"""
+    turtle.color("pink")
+    turtle.pendown()
+    count = 0
+    turtle.fillcolor("#c23e8f")
+    while count < 5:
+        turtle.begin_fill()
+        turtle.forward(155.4)
+        turtle.rt(150)
+        turtle.forward(100)
+        turtle.rt(66)
+        turtle.forward(85.05)
+        turtle.end_fill()
+        count +=1
+    count = 0
+    turtle.color("#B76E79")
+    turtle.fillcolor("#912668")
+    while count < 5:
+        turtle.begin_fill()
+        turtle.forward(155.4)
+        turtle.lt(150)
+        turtle.forward(100)
+        turtle.lt(66)
+        turtle.forward(85.05)
+        turtle.end_fill()
+        count +=1
+    turtle.penup()
 
-            
-        writer.writerow([f'# Cages will run out in month {month}'])
-       
+def draw_shuriken(turtle):
+    """ Draws a shuriken"""
+    turtle.color("black")
+    turtle.pendown()
+    count = 0
+    turtle.fillcolor("#B76E79")
+    turtle.begin_fill()
+    while count < 5:
+        turtle.forward(155.4)
+        turtle.rt(150)
+        turtle.forward(100)
+        turtle.rt(66)
+        turtle.forward(85.05)
+        count +=1
+    turtle.end_fill()
+    count =0
+    turtle.fillcolor( "white")
+    turtle.begin_fill()
+    while count<5:
+        turtle.forward(85.05)
+        turtle.lt(66)
+        turtle.forward(100)
+        turtle.lt(150)
+        turtle.forward(155.4)
+        count +=1
+    turtle.end_fill()
+    turtle.penup()
+
+
+def main():
+    '''
+    Program starts here.
+    '''
+    turt = turtle.Turtle()
+    window = turt.getscreen()
+    colors = ["white", "#F7CAC9",  "pink", "#B76E79","#a24857", "#8d1919", "#dc2f02", "#f37a48", "orange", "#f5e65e", "#bbf261", "#36D457",  "green", "#3eb489", "#5dc9b6", "#6495ed","#002366","#30106b", "#673147","purple"]
+
+    try:
+        width = int(input())
+        height = int(input())
+
+    except ValueError:
+        print('Width and height must be positive integers.')
+        return
+
+    if width < 1 or height < 1:
+        print('Width and height must be positive integers.')
+        return
+
+    get_dimentions(turt, window, width, height)
+    turt.goto(100,150)
+    count = 0
+    scale = 1
+    while scale > 0:
+        draw_hexagon(turt, colors[count], scale)
+        turt.rt(30)
+        scale -= .05
+        count += 1
+    turt.goto(-200,100)
+    draw_star(turt)
+    turt.goto(0,-150)
+    draw_shuriken(turt)
+    window.exitonclick()
+
+if __name__ == "__main__":
+    main()
